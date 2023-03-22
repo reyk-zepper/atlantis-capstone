623f4c1b07a16fc9115cdca272ffc623580f3934
Reyk Zepper <reyk.zepper@gmail.com>
2023-03-22 17:37:27 +0100

    add: delete and edit function

--- components/ProjectCards/index.js
+++ components/ProjectCards/index.js
@@ -1,48 +1,17 @@
 import styled from "styled-components";
+import ProductCard from "../ProductCard";
 
-const StyledProjectCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  border: ${({ hasData }) => (hasData ? "1px solid black" : "none")};
-  border-radius: 10px;
-  padding: 10px;
-  margin: 10px;
-`;
-
-export default function ProjectCards({ projects }) {
+export default function ProjectCards({ projects, handleDelete }) {
   const hasData = projects && projects.length > 0;
 
   return (
     <>
       {hasData && (
-        <div>
-          {projects.map((project, index) => {
-            let totalPrice = 0;
-            return (
-              <StyledProjectCard hasData={hasData} key={index}>
-                <h2>Project: {project.name}</h2>
-                <ul>
-                  {project.items.map((item) => {
-                    totalPrice += Number(item.price);
-                    return (
-                      <li key={item.id}>
-                        <p>
-                          {item.value === ""
-                            ? item.name.toUpperCase()
-                            : item.value.toUpperCase()}
-                          : {item.price}$
-                        </p>
-                      </li>
-                    );
-                  })}
-                </ul>
-                <p>Total: {totalPrice}$</p>
-              </StyledProjectCard>
-            );
-          })}
-        </div>
+        <ProductCard
+          hasData={hasData}
+          handleDelete={handleDelete}
+          projects={projects}
+        />
       )}
     </>
   );
