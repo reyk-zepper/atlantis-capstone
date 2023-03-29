2f1891576bdc60c984b44f0f8815730556375af4
Reyk Zepper <reyk.zepper@gmail.com>
2023-03-29 18:07:58 +0200

    add minimal styling

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,35 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <p>start a new project</p>
-      <Link href={"/AddProject"}>&oplus;</Link>
-    </div>
+    <StyledStartingContainer>
+      <StyledParagraph>start a new project</StyledParagraph>
+      <StyledLink href={"/AddProject"}>&oplus;</StyledLink>
+    </StyledStartingContainer>
   );
 }
+
+const StyledStartingContainer = styled.div`
+  border: solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+`;
+
+const StyledParagraph = styled.p`
+  font-weight: bold;
+  font-size: 2rem;
+  padding: 0;
+  margin: 0;
+`;
+
+const StyledLink = styled(Link)`
+  color: inherit;
+  text-decoration: none;
+  position: relative;
+  font-size: 3rem;
+  cursor: pointer;
+  &:hover {
+    color: hotpink;
+  }
+`;
