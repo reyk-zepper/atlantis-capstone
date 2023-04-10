ac61957f10a029d746a43b5d3b5f7b1e8ade3b62
Reyk Zepper <reyk.zepper@gmail.com>
2023-04-10 19:06:13 +0200

    add style to h1

--- components/Heading/index.js
+++ components/Heading/index.js
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 const Heading = styled.h1`
   text-align: center;
+  height: 2rem;
 `;
 export default Heading;
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -5,7 +5,9 @@ import styled from "styled-components";
 export default function Layout({ children }) {
   return (
     <AppContainer>
-      <Heading>p[al[ul</Heading>
+      <StyledHeadingContainer>
+        <StyledHeading>p[al[ul</StyledHeading>
+      </StyledHeadingContainer>
       <StyledMain>{children}</StyledMain>
       <Navigation />
     </AppContainer>
@@ -24,4 +26,20 @@ const StyledMain = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
+const StyledHeading = styled(Heading)`
+  color: #fff;
+  text-shadow: 0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff,
+    0 0 42px rgba(255, 214, 243, 0.6), 0 0 82px rgba(255, 214, 243, 0.6),
+    0 0 92px rgba(255, 214, 243, 0.6), 0 0 102px rgba(255, 214, 243, 0.6),
+    0 0 151px rgba(255, 214, 243, 0.6);
+`;
+
+const StyledHeadingContainer = styled.div`
+  padding: 5rem;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  width: 50rem;
+  min-height: 5rem;
 `;
