9a7e41a09c5511fdf06914d46b0eff912233ace2
Reyk Zepper <reyk.zepper@gmail.com>
2023-04-11 15:44:25 +0200

    delete html2pdf

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -10,7 +10,7 @@ import { createChartData } from "../../helper/createChartData";
 import useDarkMode from "@/hooks/useDarkMode";
 import { sumTotalPrice } from "../../helper/sumTotalPrice";
 import formatTime from "@/helper/formatTime";
-import html2pdf from "html2pdf.js";
+
 import { useEffect, useState } from "react";
 
 export default function ProductCard({ project, editState }) {
@@ -18,11 +18,6 @@ export default function ProductCard({ project, editState }) {
   const router = useRouter();
   const [moveToActive] = useStore((state) => [state.moveToActive]);
   const labelColor = isDarkMode ? "white" : "black";
-  const [isPdfActive, setIsPdfActive] = useState(false);
-
-  useEffect(() => {
-    setIsPdfActive(false);
-  }, []);
 
   if (!project) {
     return <h2>something went wrong</h2>;
@@ -33,54 +28,32 @@ export default function ProductCard({ project, editState }) {
     moveToActive(id);
   };
 
-  const handleGeneratePDF = () => {
-    const element = document.getElementById("detailsPDF");
-    const options = {
-      margin: 0.5,
-      filename: `${project.id}.pdf`,
-      image: {
-        type: "jpeg",
-        quality: 0.98,
-      },
-      html2canvas: {
-        scale: 2,
-      },
-      jsPDF: {
-        unit: "cm",
-        format: "a4",
-        orientation: "portrait",
-      },
-    };
-    setIsPdfActive(true);
-    router.push("/done");
-    html2pdf().set(options).from(element).outputPdf("dataurlnewwindow");
-  };
-
   return (
     <StyledProjectCard key={project.id}>
-      <PdfWrapper isPdfActive={isPdfActive} id={"detailsPDF"}>
-        <h2>Project: {project.name}</h2>
-        <p>created: {project.creationDate}</p>
-        <Bar
-          data={createChartData(project)}
-          options={chartOptions(false, labelColor)}
-        />
-        <ul>
-          {project.items.map((item) => {
-            return (
-              <li key={item.id}>
-                <p>{item.name.toUpperCase()}</p>
-                <p>
-                  {item.value.toUpperCase()}: {formatToEUR(item.price)}
-                </p>
-              </li>
-            );
-          })}
-        </ul>
-        <p>Total: {formatToEUR(sumTotalPrice(project))}</p>
-        <p>Working Time: {formatTime(project.workingTime)}</p>
-      </PdfWrapper>
-      {editState === "active" && <Link href={`/edit/${project.id}`}>edit</Link>}
+      <h2>Project: {project.name}</h2>
+      <p>created: {project.creationDate}</p>
+      <Bar
+        data={createChartData(project)}
+        options={chartOptions(false, labelColor)}
+      />
+      <ul>
+        {project.items.map((item) => {
+          return (
+            <li key={item.id}>
+              <p>{item.name.toUpperCase()}</p>
+              <p>
+                {item.value.toUpperCase()}: {formatToEUR(item.price)}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+      <p>Total: {formatToEUR(sumTotalPrice(project))}</p>
+      <p>Working Time: {formatTime(project.workingTime)}</p>
+
+      {editState === "active" && (
+        <StyledEditLink href={`/edit/${project.id}`}>edit</StyledEditLink>
+      )}
       {editState === "done" && (
         <>
           <button type="button" onClick={() => handleMoveToActive(project.id)}>
@@ -116,15 +89,4 @@ const StyledEditLink = styled(Link)`
     text-decoration: none;
     border: 1px solid hotpink;
   }
-`;
-
-const PdfWrapper = styled.div`
-  ${(props) =>
-    props.isPdfActive
-      ? css`
-          color: black;
-        `
-      : css`
-          color: white;
-        `}
 `;
